1254e657989d2d3e78d1fd27ec77a9f1ca10c28e
fazza7777 <aungbarlaay7777@gmail.com>
2022-11-10 20:52:47 +0630

    'search_container_ui'

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,48 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:ListView(
-        children: const[
-          HomeAppBar()
+      body: ListView(
+        children: [
+          const HomeAppBar(),
+          Container(
+            height: 500, // tempory height
+            padding: const EdgeInsets.only(top: 15),
+            decoration: const BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 50,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none, hintText: 'Search here...'),
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(Icons.camera_alt,size: 27, color: Color(0xFF4C53A5),)
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
