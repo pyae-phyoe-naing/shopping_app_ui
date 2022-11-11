3f5ada8a09562d6e27d50fb8dedf997c742aef04
fazza7777 <aungbarlaay7777@gmail.com>
2022-11-11 11:51:53 +0630

    'cart_item_list_ui'

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_ap_ui/widget/cart_app_bar.dart';
 
+import '../widget/cart_item_widget.dart';
+
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
 
@@ -12,12 +14,59 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-          children:  [
-            const CartApBar(),
-            Container()
-          ],
-        ),
+      body: ListView(
+        children: [
+          const CartApBar(),
+          Container(
+            // tempory height
+            // height: 700,
+            padding: const EdgeInsets.only(top: 15),
+            decoration: const BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                // ---- Cart Item List --- //
+                const CartItemWidget(),
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                  padding: const EdgeInsets.all(10),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.circular(10),
+                  // ),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF4C53A5),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Icon(Icons.add, color: Colors.white),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Add Coupon Code',
+                          style: TextStyle(
+                            color: Color(0xFF4C53A5),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
